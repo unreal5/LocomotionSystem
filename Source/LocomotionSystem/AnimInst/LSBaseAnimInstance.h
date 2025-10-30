d3978e87bed5896f34b7b0315c056746fd06ee98
lingbao <lingbao@outlook.com>
2025-10-30 09:34:58 +0800

    0005. Animation Blueprint and Idle Animation

---
+++ Source/LocomotionSystem/AnimInst/LSBaseAnimInstance.h
@@ -0,0 +1,16 @@
+// 版权没有，拷贝自由。
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "LSBaseAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class LOCOMOTIONSYSTEM_API ULSBaseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
